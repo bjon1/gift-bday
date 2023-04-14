a21dcccb15c661b14258fd3b075d81831310f137
nebjamin <jonb1@newpaltz.edu>
2023-04-14 19:24:10 -0400

    Add Bday Message

--- alecks/src/App.jsx
+++ alecks/src/App.jsx
@@ -1,12 +1,69 @@
 import Gift from './components/Gift';
 import './App.css'
+import { useState, useEffect } from 'react';
+import './confetti.js';
 
 function App() {
 
+  const [wasClicked, setWasClicked] = useState(false);
+
+  const updateWasClicked = () => {
+    setWasClicked(true);
+  }
+
+  useEffect(() => {
+    console.log("outside UseEffect");
+    if(wasClicked){
+      console.log("inside useEffect");
+      const start = () => {
+        console.log("inside start");
+        setTimeout(function() {
+          console.log("inside timeout");
+            start()
+        }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
+      };
+  
+      //  Stop
+  
+      const stop = () => {
+          setTimeout(function() {
+              stop()
+          }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
+      };
+  
+      start();
+      stop();
+    }
+  })
 
   return (
-    <div className="App">
-      <Gift />
+    <div className={`App ${wasClicked ? 'transition' : ' '}`}>
+      <Gift update={updateWasClicked}/>
+      {wasClicked && 
+        <div className="bday">
+          <div className="columns">
+            <div className="column has-text-centered">
+              <div class="columns is-flex-direction-column">
+                <div class="column">
+                  <div className="title is-2" id="message">Happy Birthday Alex!</div>
+                  <p><i>"congrats on turning 14"</i> - Kat</p>
+                  <p><i>"happy bday"</i> - Ben</p>
+                  <p><i>""</i> - Justin</p>
+                  <p><i>""</i> - Syed</p>
+                </div>
+                <div class="column">
+                  <img src="some image"/>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <img src='https://media.giphy.com/media/TEkr9oBZ57KFmMWScZ/giphy.gif'/> 
+            </div>
+          </div>
+
+          
+        </div>
+      }
     </div>
   )
 }
--- alecks/src/components/Gift.jsx
+++ alecks/src/components/Gift.jsx
@@ -1,30 +1,20 @@
 
 import { useState } from 'react';
 
-const Gift = () => {
+const Gift = ({update}) => {
 
     const [isClicked, setisClicked] = useState(false);
-    const [isHover, setIsHover] = useState(false);
 
     const click = isClicked ? 'click fade' : ' ';
-    const hover = isHover ? 'hover' : ' ';
 
     const openGift = () => {
         console.log("clicked");
-        setisClicked(!isClicked); //flips the state of the switch case, true for rotate and false for not rotate.
-        setIsHover(false);
-    }
-
-    const hoverGift = () => {
-        setIsHover(true);
-    }
-
-    const leaveGift = () => {
-        setIsHover(false);
+        update();
+        setisClicked(true); //flips the state of the switch case, true for rotate and false for not rotate.
     }
 
     return(
-        <div className={`gift ${click} ${hover}`} onClick={openGift} onMouseEnter={hoverGift} onMouseLeave={leaveGift}>
+        <div className={`gift ${click}`} onClick={openGift}>
             <div className="gift_bow"></div>
             <div className="gift_box"></div>
             <div>
