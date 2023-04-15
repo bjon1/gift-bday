1acfb3c6950438ecfe8ea5f2ee7114052c110676
nebjamin <jonb1@newpaltz.edu>
2023-04-14 20:06:07 -0400

    Add Confetti

--- alecks/src/App.jsx
+++ alecks/src/App.jsx
@@ -1,7 +1,7 @@
 import Gift from './components/Gift';
+import Confetti from './components/Confetti';
 import './App.css'
 import { useState, useEffect } from 'react';
-import './confetti.js';
 
 function App() {
 
@@ -11,47 +11,23 @@ function App() {
     setWasClicked(true);
   }
 
-  useEffect(() => {
-    console.log("outside UseEffect");
-    if(wasClicked){
-      console.log("inside useEffect");
-      const start = () => {
-        console.log("inside start");
-        setTimeout(function() {
-          console.log("inside timeout");
-            start()
-        }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
-      };
-  
-      //  Stop
-  
-      const stop = () => {
-          setTimeout(function() {
-              stop()
-          }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
-      };
-  
-      start();
-      stop();
-    }
-  })
-
   return (
     <div className={`App ${wasClicked ? 'transition' : ' '}`}>
       <Gift update={updateWasClicked}/>
       {wasClicked && 
-        <div className="bday">
+        <div className="bday">  
+        <Confetti />
           <div className="columns">
             <div className="column has-text-centered">
-              <div class="columns is-flex-direction-column">
-                <div class="column">
-                  <div className="title is-2" id="message">Happy Birthday Alex!</div>
+              <div className="columns is-flex-direction-column">
+                <div className="column" id="message">
+                  <div className="title is-2">Happy Birthday Alex!</div>
                   <p><i>"congrats on turning 14"</i> - Kat</p>
                   <p><i>"happy bday"</i> - Ben</p>
                   <p><i>""</i> - Justin</p>
                   <p><i>""</i> - Syed</p>
                 </div>
-                <div class="column">
+                <div className="column">
                   <img src="some image"/>
                 </div>
               </div>
---
+++ alecks/src/components/Confetti.jsx
@@ -0,0 +1,29 @@
+const Confetti = () => {
+    return(
+        <div className="container">
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+           <div className="confetti"></div>
+        </div>
+    )
+
+}
+
+export default Confetti;
