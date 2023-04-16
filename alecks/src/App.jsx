13e52c117b64d4de21ea25cd762289c96f826073
nebjamin <jonb1@newpaltz.edu>
2023-04-16 00:14:41 -0400

    Add Warm Messages

--- alecks/src/App.jsx
+++ alecks/src/App.jsx
@@ -24,10 +24,11 @@ function App() {
                 <img src='https://media.giphy.com/media/TEkr9oBZ57KFmMWScZ/giphy.gif'/>
                 <div class="cta__text-column">
                   <h2 className="title is-2">Happy Birthday Alex!</h2>
-                  <p>"congrats on turning 14" - Kat</p>
-                  <p>"happy bday" - Ben</p>
-                  <p>"" - Justin</p>
-                  <p>"" - Syed</p>
+                  <p>"congrats on turning 14" - <i>Kat</i></p>
+                  <p>"Happy birthday Alex, I hope you like this gift" - <i>Ben</i></p>
+                  <p>"Feliz Cumpleaños! Thanks for sharing the same dum humor of a human rat dragging pizza" - <i>Kenia</i></p>
+                  <p>"Happy Bday buddy thanks for being the bestest greatest friend. UwU Lots of love and kisses" - <i>Justin</i></p>
+                  <p>"Happy Bday from a fellow gumi wuewuewuewuewue" - <i>Syed</i></p>
                 </div>
               </article>
             </div>
